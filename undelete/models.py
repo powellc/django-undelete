9c9f72d57bb0de93909b530471c9e5493603302b
Charl P. Botha <cpbotha@timescapers.com>
2011-07-25 20:08:57 +0200

    Changed all tabs to 4 spaces, which PEP8 favours.

--- undelete/models.py
+++ undelete/models.py
@@ -5,19 +5,19 @@ from django.utils.translation import ugettext_lazy as _
 from undelete.managers import TrashedManager, NonTrashedManager
 
 class TrashableMixin(models.Model):
-	trashed_at = models.DateTimeField(_('Trashed'), editable=False, blank=True, null=True)
+    trashed_at = models.DateTimeField(_('Trashed'), editable=False, blank=True, null=True)
 
-	objects = NonTrashedManager()
-	trash = TrashedManager()
+    objects = NonTrashedManager()
+    trash = TrashedManager()
 
-	def delete(self, *args, **kwargs, trash=True):
-	    if not self.trashed_at and trash:
-	        self.trashed_at = datetime.now()
-	        self.save()
-	    else:
-			super(SomeModel, self).delete(*args, **kwargs)
+    def delete(self, *args, **kwargs, trash=True):
+        if not self.trashed_at and trash:
+            self.trashed_at = datetime.now()
+            self.save()
+        else:
+            super(SomeModel, self).delete(*args, **kwargs)
 
-	def restore(self, commit=True):
-		self.trashed_at = None
-		if commit:
-			self.save()
+    def restore(self, commit=True):
+        self.trashed_at = None
+        if commit:
+            self.save()
