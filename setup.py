9929c36bbf355a40775a826413d8a19d198d01fb
Charl P. Botha <cpbotha@timescapers.com>
2011-07-25 20:09:50 +0200

    Changed package name to django-undelete.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name = "undelete",
+    name = "django-undelete",
     version = "0.1",
     packages = find_packages(),
     install_requires = ['django>=1.3'],
