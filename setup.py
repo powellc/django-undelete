e70f86624485bdb283156afc6f84ec0b29136cb8
Colin Powell <colin.powell@gmail.com>
2013-12-04 17:45:31 -0500

    Upgrading version in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "django-undelete",
-    version = "0.1",
+    version = "1.0.0",
     packages = find_packages(),
     install_requires = ['django>=1.3'],
 )
